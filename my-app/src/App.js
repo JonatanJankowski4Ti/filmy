3c652d3ccab83e24eb0e855769b85ff509b03325
JonatanJankowski4Ti <jonatanjankowski1234@gmail.com>
2024-09-27 11:11:23 +0200

    listy rozwijalnej i jej etykiety o treści „Rodzaj” ‒ przycisku „Dodaj” ‒ Lista rozwijalna ma 5 opcji: ‒ pustą ‒ Komedia o wartości 1 ‒ Obyczajowy o wartości 2 ‒ Sensacyjny o wartości 3 ‒ Horror o wartości 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -8,5 +7,17 @@ function App() {
         <div class="mb-3">
           <label for="film" class="form-label">Tytuł filmu</label>
           <input type="text" class="form-control" id="film" />
-    </div>
+          </div>
+         <select name="Rodzaj">
+          <option value="1"> Komedia</option>
+          <option value="2"> Obyczajowy </option>
+          <option value="3"> Sensacyjny </option>
+          <option value="4"> Horror </option>
+         </select>
+         <button type="button">Dodaj</button>
     </form>
+    </div>
+     );
+}
+
+export default App;
